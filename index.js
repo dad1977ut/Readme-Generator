1a09df26da3733cc902bc94c96b7ca0a2959ac84
Dameon Davis <dad1977ut@yahoo.com>
2021-02-17 00:50:19 -0700

    added to files

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const generater = require("./utils/generateMarkdown.js");
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is the description of the project?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Who Contribute to the project",
+      name: "contribute",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    console.log(generater(response));
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Contributors
+${data.contribute}
 `;
 }
 
