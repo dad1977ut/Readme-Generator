6ea10c8ebbf5065a018fc4e7aaefc476db9cbc14
Dameon Davis <dad1977ut@yahoo.com>
2021-02-18 01:24:13 -0700

    added table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Contributors](#contributors)
+  4. [Questions](#questions)
 ## Description
 ${data.description}
 ## Installation
